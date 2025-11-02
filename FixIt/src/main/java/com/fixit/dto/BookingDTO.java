8c83a83fd649f4687bdf30addd61bc30fa8743a7
Yasindu Udayanga <yasinduudayanga2003@gmail.com>
2025-11-02 21:30:06 +0530

    add BookingDTO & ServiceDTO files

---
+++ FixIt/src/main/java/com/fixit/dto/BookingDTO.java
@@ -0,0 +1,18 @@
+package com.fixit.dto;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class BookingDTO {
+    @NotNull
+    private Long providerId;
+
+    @Future  // Must be future date
+    @NotNull
+    private LocalDateTime dateTime;
+
+    private String remarks;  // Optional
+}
